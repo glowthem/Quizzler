bc35baa91c06140dfe2a0b3826d26e1c035e2417
glowthem <glowing713@naver.com>
2020-07-01 16:50:05 +0900

    2020-07-01 🔧 Re-type to review quizzler

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -7,45 +7,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    
     let quiz = [
-        ["4 + 2 is equal to 6", "True"],
-        ["5 - 3 is greater than 1", "True"],
-        ["3 + 8 is less than 10", "False"]
-    ]
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ];
     
-    var questionNumber = 0
+    var questionNumber = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        updateUI()
-        
+        updateUI();
     }
 
-    
-    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber][1]
-        
-        if userAnswer == actualAnswer {
-            print("Right!")
-        } else {
-            print("Wrong!")
-        }
-        
-        if questionNumber + 1 < quiz.count {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-        }
-        updateUI()
+        questionNumber = (questionNumber + 1)%quiz.count;
+        updateUI();
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber];
     }
     
 }
