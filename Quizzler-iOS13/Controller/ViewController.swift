55ee72953e3081d8034dc1e166412a12d6c61676
glowthem <glowing713@naver.com>
2020-07-03 17:17:06 +0900

    2020-07-03 ➕ imply MVC pattern && make quizzler to multi-choice

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice3Button: UIButton!
+    
+    var quizBrain = QuizBrain()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        }else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        let answerChoices = quizBrain.getAnswers()
+        choice1Button.setTitle(answerChoices[0], for: .normal)
+        choice2Button.setTitle(answerChoices[1], for: .normal)
+        choice3Button.setTitle(answerChoices[2], for: .normal)
+        
+        choice1Button.backgroundColor = UIColor.clear
+        choice2Button.backgroundColor = UIColor.clear
+        choice3Button.backgroundColor = UIColor.clear
+    }
+    
+}
+
